18ff8e1372a9657def356bbc8636e05aae4a21f3
pp4ny <Punyapa_k@cmu.ac.th>
2025-05-07 16:44:52 +0700

    session 5

---
+++ subject.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer(function(req, res){
+    res.setHeader('Access-Control-Allow-Origin' , '*');
+    res.setHeader('Access-Control-Allow-Method' , 'GET , POST');
+    res.setHeader('Access-Control-Allow-Header' , 'Content-Type');
+
+    res.writeHead(200, {'Content-Type' : 'text/json'});
+    res.write('{"contactSubject": ["General Enquiry", "Class", "Schedule", "Instructor", "Price","Location" ,"Other"]}');
+    res.end();
+
+});
+server.listen(4040)
