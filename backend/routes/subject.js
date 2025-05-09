3408bfedb64a414e4e9f78c9168438c767a78bed
pp4ny <Punyapa_k@cmu.ac.th>
2025-05-09 16:36:54 +0700

    session 6

---
+++ backend/routes/subject.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const subject = require('../data/contact_subject.json')
+
+router.get('/' , (req, res) => {
+    //res.end('{"contactSubject": ["General Enquiry", "Class", "Schedule", "Instructor", "Price","Location" ,"Other" ,"Pany"]}');
+    res.json(subject);
+});
+
+module.exports = router;
--- subject.js
+++ subject.js
@@ -10,4 +10,5 @@ const server = http.createServer(function(req, res){
     res.end();
 
 });
+
 server.listen(4040)
